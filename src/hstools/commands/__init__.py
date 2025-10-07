a4e69bf6bbef33c0255437e05c1c2c0623522c47
user.name <gmail@hdknr.com>
2025-10-07 14:12:27 +0900

    show-config コマンド

--- src/hstools/commands/__init__.py
+++ src/hstools/commands/__init__.py
@@ -1,3 +1,6 @@
+import json
+from pathlib import Path
+
 import click
 import yaml
 
@@ -17,17 +20,33 @@ def get_access_token(config, portal):
 
 
 @click.group()
-@click.argument("src_portal")
+@click.option("--src_portal", "-s", default=None)
 @click.option("--dst_portal", "-d", default=None)
 @click.option("--config", default="hubspot.config.yml")
 @click.pass_context
 def main(ctx, src_portal, dst_portal, config):
     ctx.ensure_object(dict)
-    ctx.obj["config"] = load_ymal(config)
-    ctx.obj["src_potal"] = src_portal
-    ctx.obj["dst_potal"] = dst_portal or src_portal
-    ctx.obj["src_access_token"] = get_access_token(ctx.obj["config"], ctx.obj["src_potal"])
-    ctx.obj["dst_access_token"] = get_access_token(ctx.obj["config"], ctx.obj["dst_potal"])
+    if Path(config).is_file():
+        ctx.obj["config"] = load_ymal(config)
+        if not src_portal:
+            src_portal = ctx.obj["config"]["defaultPortal"]
+        ctx.obj["src_potal"] = src_portal
+        ctx.obj["dst_potal"] = dst_portal or src_portal
+        ctx.obj["src_access_token"] = get_access_token(ctx.obj["config"], ctx.obj["src_potal"])
+        ctx.obj["dst_access_token"] = get_access_token(ctx.obj["config"], ctx.obj["dst_potal"])
+    else:
+        ctx.obj["config"] = {}
+        ctx.obj["src_potal"] = src_portal
+        ctx.obj["dst_potal"] = dst_portal or src_portal
+        ctx.obj["src_access_token"] = None
+        ctx.obj["dst_access_token"] = None
+
+
+@main.command()
+@click.pass_context
+def show_config(ctx):
+    """hubspot config の表示"""
+    print(json.dumps(ctx.obj["config"], indent=2))
 
 
 main.add_command(html.html)
